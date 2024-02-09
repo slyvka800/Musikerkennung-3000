bbd2b1bfdd125d73e21b18f91f6c051a2b669bea
slyvka800 <p.a.slyvka@gmail.com>
2024-02-09 10:42:04 +0100

    adding fingerprint and spectrogram functions

--- build/lib/abracadabra/classes/fingerprint_class.py
+++ build/lib/abracadabra/classes/fingerprint_class.py
@@ -25,6 +25,28 @@ class Fingerprinting:
             self.database = {}  # ...database to store hashes
 
     @staticmethod
+    def my_spectrogram(audio):
+        """Helper function that performs a spectrogram with the values in settings."""
+        nperseg = int(settings.SAMPLE_RATE * settings.FFT_WINDOW_SIZE)
+        return spectrogram(audio, settings.SAMPLE_RATE, nperseg=nperseg)
+
+    @staticmethod
+    def file_to_spectrogram(filename):
+        """Calculates the spectrogram of a file.
+
+        Converts a file to mono and resamples to :data:`~abracadabra.settings.SAMPLE_RATE` before
+        calculating. Uses :data:`~abracadabra.settings.FFT_WINDOW_SIZE` for the window size.
+
+        :param filename: Path to the file to spectrogram.
+        :returns: * f - list of frequencies
+                * t - list of times
+                * Sxx - Power value for each time/frequency pair
+        """
+        a = AudioSegment.from_file(filename).set_channels(1).set_frame_rate(settings.SAMPLE_RATE)
+        audio = np.frombuffer(a.raw_data, np.int16)
+        return Fingerprinting.my_spectrogram(audio)
+
+    @staticmethod
     def find_peaks(Sxx):
         data_max = maximum_filter(Sxx, size=settings.PEAK_BOX_SIZE, mode='constant', cval=0.0)
         peak_goodmask = (Sxx == data_max)
@@ -94,6 +116,21 @@ class Fingerprinting:
         return matched_songs
     
 
+    @staticmethod
+    def fingerprint_file(filename):
+        """Generate hashes for a file.
+
+        Given a file, runs it through the fingerprint process to produce a list of hashes from it.
+
+        :param filename: The path to the file.
+        :returns: The output of :func:`hash_points`.
+        """
+        f, t, Sxx = Fingerprinting.file_to_spectrogram(filename)
+        peaks = Fingerprinting.find_peaks(Sxx)
+        peaks = Fingerprinting.idxs_to_tf_pairs(peaks, t, f)
+        return Fingerprinting.hash_points(peaks, filename)
+    
+
 
 if __name__ == "__main__":
     
@@ -102,6 +139,8 @@ if __name__ == "__main__":
     audio = AudioSegment.from_wav(audio_file)
 
     print(fp.fingerprint_file(audio_file))
+    print("our function:")
+    print(Fingerprinting.fingerprint_file(audio_file))
 
 #     # Converts audio to raw data
 #     audio_raw = audio.raw_data
