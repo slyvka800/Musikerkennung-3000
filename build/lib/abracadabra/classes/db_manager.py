ddc85016f5e1f81c3492f0731059aef0ab19824f
slyvka800 <p.a.slyvka@gmail.com>
2024-02-12 10:56:04 +0100

    added database manager class

---
+++ build/lib/abracadabra/classes/db_manager.py
@@ -0,0 +1,22 @@
+from tinydb import TinyDB
+
+class DataBaseManager:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(DataBaseManager, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+        
+    def __init__(self):
+        if not hasattr(self, 'db'):
+            self.db = TinyDB("music_hashes.json")
+        if not hasattr(self, 'table'):
+            self.table = self.db.table("hashes")
+
+    def insert(self, data):
+        self.table.insert(data)
+    
+    def get(self, query):
+        return self.table.search(query)
+
--- build/lib/abracadabra/test.py
+++ build/lib/abracadabra/test.py
@@ -1,3 +1,7 @@
 import fingerprint as fp
+from classes.db_manager import DataBaseManager
 
 print(fp.fingerprint_file("../../../Samples/test.wav"))
+
+db_manager = DataBaseManager()
+db_manager.insert({"test.wav": fp.fingerprint_file("../../../Samples/test.wav")})
