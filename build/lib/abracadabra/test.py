8600a709c0f6dc52295193621035201ecd5fc761
vella <vellaclimbs@gmail.com>
2024-02-14 17:50:57 +0100

    in test.py added compare

--- build/lib/abracadabra/test.py
+++ build/lib/abracadabra/test.py
@@ -1,11 +1,21 @@
 from tinydb import Query
 import fingerprint as fp
 from classes.db_manager import DataBaseManager
-
+from classes.recogniser_class import Recogniser
 print(fp.fingerprint_file("../../../Samples/test.wav"))
 
 query = Query()
 
 db_manager = DataBaseManager()
 db_manager.insert({'name': 'test.wav', 'hash': fp.fingerprint_file("../../../Samples/test.wav")})
-db_manager.table.remove()
+#db_manager.table.delete()
+
+recogniser_class_ = Recogniser()
+print("compare function")
+
+#db_manager.insert({'name': 'test_cpy.wav', 'hash': fp.fingerprint_file("../../../Samples/test.wav")})
+
+fingerprint_hash = fp.fingerprint_file("../../../Samples/test.wav")
+
+data = recogniser_class_.compare()
+print(data)
