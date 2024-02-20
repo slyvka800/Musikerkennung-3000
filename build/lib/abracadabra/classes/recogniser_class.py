61146e53a9c1c254be699de1f551c9d80106c62d
vella <vellaclimbs@gmail.com>
2024-02-20 14:17:07 +0100

    just a few changes

--- build/lib/abracadabra/classes/recogniser_class.py
+++ build/lib/abracadabra/classes/recogniser_class.py
@@ -31,16 +31,6 @@ class Recogniser:
         self.fingerprinting = Fingerprinting
 
     def score_match(self, offsets):
-        """Score a matched song.
-
-        Calculates a histogram of the deltas between the time offsets of the hashes from the
-        recorded sample and the time offsets of the hashes matched in the database for a song.
-        The function then returns the size of the largest bin in this histogram as a score.
-
-        :param offsets: List of offset pairs for matching hashes
-        :returns: The highest peak in a histogram of time deltas
-        :rtype: int
-        """
         # Use bins spaced 0.5 seconds apart
         binwidth = 0.5
         tks = list(map(lambda x: x[0] - x[1], offsets))
@@ -51,15 +41,7 @@ class Recogniser:
         return np.max(hist)
 
     def best_match(self, matches):
-        """For a dictionary of song_id: offsets, returns the best song_id.
-
-        Scores each song in the matches dictionary and then returns the song_id with the best score.
-
-        :param matches: Dictionary of song_id to list of offset pairs (db_offset, sample_offset)
-        as returned by :func:`~abracadabra.Storage.storage.get_matches`.
-        :returns: song_id with the best score.
-        :rtype: str
-        """
+ 
         matched_song = None
         best_score = 0
         for song_id, offsets in matches.items():
@@ -73,15 +55,7 @@ class Recogniser:
         return matched_song
     
     def recognise_song(self, filename):
-        """Recognises a pre-recorded sample.
 
-        Recognises the sample stored at the path ``filename``. The sample can be in any of the
-        formats in :data:`recognise.KNOWN_FORMATS`.
-
-        :param filename: Path of file to be recognised.
-        :returns: :func:`~abracadabra.recognise.get_song_info` result for matched song or None.
-        :rtype: tuple(str, str, str)
-        """
         hashes = self.fingerprinting.fingerprint_file(filename)
         matches = self.db_manager.get_matches(hashes)
         matched_song = self.best_match(matches)
@@ -90,10 +64,10 @@ class Recogniser:
             return info
         return matched_song
 
-    def compare(self):
-        matches = self.db_manager.get_all_hashes()
-        return_data = self.best_match(matches)
-        return return_data
+    # def compare(self):
+    #     matches = self.db_manager.get_all_hashes()
+    #     return_data = self.best_match(matches)
+    #     return return_data
 
         
             
